2a96776b96c03e88ec0f780779545a1159ae18fd
Max Luetkemeyer <max.luetkemeyer@outlook.de>
2024-01-25 23:36:56 +0000

    env update

--- src/env.mjs
+++ src/env.mjs
@@ -26,7 +26,7 @@ export const env = createEnv({
      * `NEXT_PUBLIC_`.
      */
     client: {
-        NEXT_PUBLIC_LLM_WEBSOCKET_URL: z.string(),
+        NEXT_PUBLIC_LLM_WEBSOCKET_URL: z.string().url(),
         // NEXT_PUBLIC_CLIENTVAR: z.string(),
     },
 
@@ -37,7 +37,7 @@ export const env = createEnv({
     runtimeEnv: {
         DATABASE_URL: process.env.DATABASE_URL,
         NODE_ENV: process.env.NODE_ENV,
-        NEXT_PUBLIC_LLM_WEBSOCKET_URL: process.env.LLM_WEBSOCKET_URL,
+        NEXT_PUBLIC_LLM_WEBSOCKET_URL: process.env.NEXT_PUBLIC_LLM_WEBSOCKET_URL,
         DATA_APP_URL: process.env.DATA_APP_URL,
         // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
     },
